e9520e4d60e02a6b061c8c2e43938f1c1c1c0a36
laert-iskurti <iskurtilaert@gmail.com>
2021-03-14 19:26:47 +0100

    Login/Signup

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 app_name = 'game'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('logout/', views.logoutUser, name='logout')
+    
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import *
+from .forms import ExtendedUserCreationForm, UserProfileForm
+
+def registerPage(request):
+    if request.method == "POST":
+        form = ExtendedUserCreationForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+                   
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+	# user = form.cleaned_data.get('username')
+	# messages.success(request, 'Account was created for ' + user)
+          
+            return redirect('game:login')
+    else:
+        form = ExtendedUserCreationForm()
+        profile_form = UserProfileForm()	
+    
+    context = {'form':form, 'profile_form': profile_form}
+    return render(request, 'game/register.html', context)
 
 
-def index(request):
-    context = {'message': "Welcome to the game"}
-    return render(request, 'game/index.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+	    return redirect('game:home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password =request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('game:home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+        
+        context = {}
+        return render(request, 'game/login.html', context)
+
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('game:login')
+
+
+
+@login_required(login_url='game:login')
+def home(request):
+    context={'message': 'Welcome to the main Page'}
+    return render(request, 'game/main.html', context)
