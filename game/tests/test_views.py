18e751affa6b811bef9972df069e4d60c78ad272
Amartya Vats <amartyavats@Amartyas-MBP.students.jacobs-university.de>
2021-04-06 11:12:39 +0200

    added new test cases

--- game/tests/test_views.py
+++ game/tests/test_views.py
@@ -13,8 +13,31 @@ class TestViews(TestCase):
 
     def test_home(self):
         response = self.client.get(reverse('game:home'), follow=True)
-        
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'game/main.html')
 
-        
+    # New test cases
+    def test_register(self):
+        responce = self.client.get(reverse('game:register'), follow=True)
+        self.assertEquals(responce.status_code, 200)
+
+    def test_login(self):
+        responce = self.client.get(reverse('game:login'), follow=True)
+        self.assertEquals(responce.status_code, 200)
+
+    def test_logout(self):
+        responce = self.client.get(reverse('game:logout'), follow=True)
+        self.assertEquals(responce.status_code, 200)
+
+    def test_accountSettings(self):
+        response = self.client.get(reverse('game:accountSettings'), follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'game/accountSettings.html')
+
+    def test_create_game(self):
+        responce = self.client.get(reverse('game:create_game'), follow=True)
+        self.assertEquals(responce.status_code, 200)
+
+    def test_join(self):
+        response = self.client.get(reverse('game:join'), follow=True)
+        self.assertEquals(response.status_code, 200)
